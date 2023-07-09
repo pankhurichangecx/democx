00375ca68d40f1ffaefbb28e0da91cebde093670
Pankhuri <pankhuri.garg@changecx.com>
2023-07-09 18:01:55 +0530

    Added code

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      margin:{
+      '75': '75px',
+      }
+    },
+  },
+  plugins: [],
+}
+
